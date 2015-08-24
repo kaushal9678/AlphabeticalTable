4a525910b194a0387ea3273adf2e68730c0a0c77
kaushal9678 <k.kaushal50@gmail.com>
2015-08-24 09:26:05 +0530

    Initial Commit

---
+++ AlphabeticalTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlphabeticalTable
+//
+//  Created by mac on 8/24/15.
+//  Copyright (c) 2015 vstacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlphabeticalTable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AlphabeticalTable
+//
+//  Created by mac on 8/24/15.
+//  Copyright (c) 2015 vstacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
